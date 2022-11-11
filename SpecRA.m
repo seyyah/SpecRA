c4cb6746afcb747158bad790357e7947cd1f68c6
Forrest Webler, PhD <46048820+fwebler@users.noreply.github.com>
2022-11-11 10:54:51 +0100

    Update SpecRA.m

--- SpecRA.m
+++ SpecRA.m
@@ -3,19 +3,21 @@
 %
 % inputs: A reference high-dimensional library L
 %         A "measured" library M = g(L) where g is a model of the sensor (can be relearned)
-%         A the raw output from the sensor denoted input = [R, G, B]'
+%         The luminous efficacy function V
+%         A the raw output from the sensor denoted RGB = [R, G, B]'
+%         The illuminance from LYS lx
 %
 % outputs: The reconstructed spectrum W in 5nm intervals 
 % 
 % written by Forrest Webler fwebler@gmail.com
 % based in part on https://doi.org/10.3390/s22062377
 
-function W = SpecRA_8(L,M,input)
+function W = SpecRA_8(L,M,V,RGB,lx)
 
     wls = 380:5:780'; % fixed wavelengths from L
 
     % power normalize the input 
-    p=input/sum(input); 
+    p=RGB/sum(input); 
 
     % power normalize the reference libraries
     for i=1:size(M,2)
@@ -29,10 +31,14 @@ function W = SpecRA_8(L,M,input)
 
     % reconstruct spectrum from high-dimensional library
     W = sum(x_b'.*L0,2);
+    W(:,2) = W(:,2)/sum(W(:,2));
+    
+    % recover absolute irradiance units (W/m2) 
+    Km = 683; % lm/W
+    c = lx/(Km*sum(V.*W(:,2)));
+    W(:,2)=c.*W(:,2);
 
     % set all negative values to 0 (this performed better than constraing the solution to positive values)
     W(W<0,1)=0; 
 
-    % power normalize to get absolute units
-    W = W/sum(W);
 end
